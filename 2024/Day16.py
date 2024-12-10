7e9f051d50e1ab6a1a6679b0a1799db53d4d45e3
Marcel Krause <32864708+marcel-krause@users.noreply.github.com>
2024-12-10 19:54:27 +0100

    Add template files for the remaining AoC days

---
+++ 2024/Day16.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.append('..')
+from Driver import load_data
+part, input_type, data_raw, data_lines, data_lines_int = load_data()
+
+# Solution to part 1
+def part_1():
+    result = 0
+
+
+    return result
+
+# Solution to part 2
+def part_2():
+    result = 0
+
+
+    return result
+
+# Print the result
+if part == 1:
+    print(part_1())
+elif part == 2:
+    print(part_2())
